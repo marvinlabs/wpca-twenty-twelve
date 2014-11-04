995edbd3a0c2d4f939590f76af0e0fe2b03f4637
Vincent Mimoun-Prat <vincent.prat@marvinlabs.com>
2014-11-04 14:21:24 +0100

    v1.0.0

--- functions.php
+++ functions.php
@@ -10,24 +10,24 @@ function cuar_theme_declare_supported_features() {
 	// Tells Customer Area that this theme is providing its own stylesheet (will hide CSS settings from the plugin 
 	// options page)
 	//--
-	add_theme_support( 'customer-area.stylesheet' );
+// 	add_theme_support( 'customer-area.stylesheet' );
 	
 	//-------------------------------------------------------------------------------------------------------------
 	// Tells Customer Area that this theme is already outputting the Customer Area navigation menu somewhere (will 
 	// hide automatic navigation menu printing settings from the plugin options page)
 	//--
 	add_theme_support( 'customer-area.navigation-menu' );
-	
+
 	//-------------------------------------------------------------------------------------------------------------
 	// Tells Customer Area that this theme is providing single post templates for the private content and 
 	// containers. The plugin will disabled the settings linked to 'single post footer' for the given post types.
 	//--
-	add_theme_support( 'customer-area.single-post-templates', array( 
-			'cuar_private_file', 	// Customize display in file single-cuar_private_file.php
-			'cuar_private_page',  	// Customize display in file single-cuar_private_page.php
-			'cuar_conversation',  	// Customize display in file single-cuar_conversation.php
-			'cuar_project',  	 	// Customize display in file single-cuar_project.php
-		) );
+//	add_theme_support( 'customer-area.single-post-templates', array( 
+//			'cuar_private_file', 	// Customize display in file single-cuar_private_file.php
+//			'cuar_private_page',  	// Customize display in file single-cuar_private_page.php
+//			'cuar_conversation',  	// Customize display in file single-cuar_conversation.php
+//			'cuar_project',  	 	// Customize display in file single-cuar_project.php
+//		) );
 	
 	//-------------------------------------------------------------------------------------------------------------
 	// Tells Customer Area that this theme is already providing its own styles and javascript files for the  
@@ -39,17 +39,17 @@ function cuar_theme_declare_supported_features() {
 	// For instance, the following line tells the plugin that we will take care of including the files required for
 	// the jQuery Select2 script. However, the plugin is still responsible for writing the markup to enable the 
 	// jQuery Select2 library on the appropriate elements.
-	add_theme_support( 'customer-area.library.jquery.select2', array( 'files' ) );
+//	add_theme_support( 'customer-area.library.jquery.select2', array( 'files' ) );
 	
 	// If we want to disable jQuery Select2 and replace it with our own alternative, we should tell the plugin that 
 	// not only we don't want it to include the files, but also that we don't want it to output any related markup.
-	add_theme_support( 'customer-area.library.jquery.select2', array( 'files', 'markup' ) );
+//	add_theme_support( 'customer-area.library.jquery.select2', array( 'files', 'markup' ) );
 	
 	// Other libraries that may be also disabled/taken care of. If we don't specify an array as a parameter, the 
 	// plugin assumes that the theme is taking care of everything for that library (files, markup, ...)
-	add_theme_support( 'customer-area.library.bootstrap.dropdown' );
-	add_theme_support( 'customer-area.library.bootstrap.transition' );
-	add_theme_support( 'customer-area.library.bootstrap.collapse' );
+//	add_theme_support( 'customer-area.library.bootstrap.dropdown' );
+//	add_theme_support( 'customer-area.library.bootstrap.transition' );
+//	add_theme_support( 'customer-area.library.bootstrap.collapse' );
 	
 	
 	//-------------------------------------------------------------------------------------------------------------
@@ -77,3 +77,44 @@ add_action( 'after_setup_theme', 'cuar_theme_declare_supported_features' );
 	return $args;
  }
  add_filter( 'cuar/core/page/nav-menu-args', 'cuar_theme_custom_nav_walker' );
+ 
+ 
+/**
+ * Load the style sheet from the parent theme.
+ */
+function cuar_enqueue_parent_theme_styles() {
+ 
+	// Enqueue the parent stylesheet
+	wp_enqueue_style( 'twentytwelve-parent-style', get_template_directory_uri() . '/style.css', array(), '0.1', 'all' );
+ 
+	// Enqueue the parent rtl stylesheet
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'twentytwelve-parent-style-rtl', get_template_directory_uri() . '/rtl.css', array(), '0.1', 'all' );
+	}
+ 
+}
+add_action( 'wp_enqueue_scripts', 'cuar_enqueue_parent_theme_styles' );
+
+/** 
+ * Add the full width body class to our private post types
+ */
+function cuar_body_class( $classes ) {
+	$post_type = get_post_type();
+	$private_post_types = array_merge(
+		CUAR_Plugin::get_instance()->get_content_post_types(),
+		CUAR_Plugin::get_instance()->get_container_post_types() );
+		
+	if ( in_array($post_type, $private_post_types) ) {
+		$classes[] = 'full-width';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'cuar_body_class' );
+ 
+ 
+ 
+ 
+ 
+ 
+ 
