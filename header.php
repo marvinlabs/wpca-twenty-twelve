de4d465bef33a8752aee6a0b857170ed314a2549
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-05-27 16:36:29 +0200

    Get back to the child theme structure

--- header.php
+++ header.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * The Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ *
  * Demonstrates how one can output the Customer Area menu on every page of Customer Area (see at the bottom of this file)
  */
 ?><!DOCTYPE html>
@@ -9,7 +17,7 @@
 <!--[if IE 8]>
 <html class="ie ie8" <?php language_attributes(); ?>>
 <![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
@@ -34,23 +42,21 @@
 		</hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Menu', 'twenty-twelve-plus' ); ?></button>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twenty-twelve-plus' ); ?>"><?php _e( 'Skip to content', 'twenty-twelve-plus' ); ?></a>
+			<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
+			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 
 		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 		<?php endif; ?>
 	</header><!-- #masthead -->
 
 	<div id="main" class="wrapper">
-
+	
 <?php
-	if (class_exists('CUAR_Plugin')
-        && (cuar_is_customer_area_page(get_queried_object_id())
-			|| cuar_is_customer_area_private_content(get_queried_object_id())))
-    {
+	if ( cuar_is_customer_area_page( get_queried_object_id() )
+			|| cuar_is_customer_area_private_content( get_queried_object_id() ) ) {
 		cuar_the_customer_area_menu();
 	}
 ?>
